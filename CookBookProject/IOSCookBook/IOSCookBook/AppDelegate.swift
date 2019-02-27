02bb7c75362206fe4e361efeaf7ec5ad1f760f3e
sunset12321215 <sunsetno08@gmail.com>
2019-02-27 14:04:21 +0700

    Initial Project

---
+++ CookBookProject/IOSCookBook/IOSCookBook/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  IOSCookBook
+//
+//  Created by mac on 2/27/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
